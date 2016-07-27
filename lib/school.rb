157af85b20ab4af2d63de920da7412cd811c74ec
Francis <roccofrancis@gmail.com>
2016-07-26 20:48:48 -0400

    Complete Lab: Make School Class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :name
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name_array|
+      @roster[grade] = name_array.sort
+    end
+    @roster
+  end
+
+end
